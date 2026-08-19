58b1062ef14ce4de3be3a9ef758264bd3d11d170
raulbfernandez <raul.b.fernandez@gmail.com>
2026-08-19 14:04:39 -0700

    Add files via upload

--- baseline-app/api/send-email.js
+++ baseline-app/api/send-email.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        from: 'Baseline Tennis <onboarding@resend.dev>',
+        from: 'Baseline Tennis <noreply@losfeliztennisclub.com>',
         to,
         subject,
         html,
